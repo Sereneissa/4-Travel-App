bf199515971f98ee66df4a09b8186403cc019d04
Sereneissa <sissa@ucsd.edu>
2021-12-15 16:54:34 -0500

    enter

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -22,19 +22,16 @@ function handleSubmit(event) {
     .then (res => {
         console.log(res)
 
-      let countDownClock = new Date(formDate).getTime()
-      let y = setInterval(function() {
-        let dateNow = new Date().getTime()
+      const countDownClock = new Date(formDate).getTime()
+      const y = setInterval(function() {
+        const dateNow = new Date().getTime()
 
-        let diffDays = countDownClock - dateNow
+        const diffDays = countDownClock - dateNow
 
-        let days = Math.floor(diffDays / (1000 * 60 * 60 *24))
-
-        let hours = Math.floor((diffDays % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-
-        let minutes = Math.floor((diffDays % (1000 * 60 * 60)) / (1000 * 60))
-
-        let seconds = Math.floor((diffDays % (1000 * 60)) / 1000)
+        const seconds = Math.floor((diffDays / 1000) % 60);
+        const minutes = Math.floor((diffDays / 1000 / 60) % 60);
+        const hours = Math.floor((diffDays / (1000 * 60 * 60)) % 24);
+        const days = Math.floor(diffDays / (1000 * 60 * 60 * 24));
 
         document.getElementById("countdown-clock").innerHTML = days + "d" + ''+ hours + "h " + minutes + "m " + seconds + "s "
 
